e1ab0a938b902e6c2240ba7b6863c26140824b30
sopanmunde <sopanmunde5@gmail.com>
2026-02-22 03:00:32 +0530

    fix gitignore

--- main.py
+++
@@ -1,8 +0,0 @@
-def main():
-    print("Hello from trishul-ai!")
-
-print("This side from Sopan Munde")
-
-
-if __name__ == "__main__":
-    main()
--- template.sh
+++ template.sh
@@ -7,7 +7,9 @@ touch src/helper.py
 touch src/prompts.py
 touch setup.py
 touch app.py
-
+touch research/trial.ipynb
+touch requirements.txt
+touch store_index.py
 
 
 echo "Project structure created successfully!"
